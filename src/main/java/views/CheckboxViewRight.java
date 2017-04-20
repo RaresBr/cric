3a2aed554c147d757873fb24cf1626304daa9b2f
RaresBr <rares.bradea@gmail.com>
2017-04-20 20:52:40 +0300

    Added dashboard backend

---
+++ src/main/java/views/CheckboxViewRight.java
@@ -0,0 +1,54 @@
+package views;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+ 
+@ManagedBean 
+public class CheckboxViewRight {
+ 
+   
+    private List<SelectItem> cars;
+    private String[] selectedCars;
+ 
+    @PostConstruct
+    public void init() {
+         
+        cars = new ArrayList<SelectItem>();
+        SelectItemGroup germanCars = new SelectItemGroup("German Cars");
+        germanCars.setSelectItems(new SelectItem[] {
+            new SelectItem("BMW", "BMW"),
+            new SelectItem("Mercedes", "Mercedes"),
+            new SelectItem("Volkswagen", "Volkswagen")
+        });
+         
+        SelectItemGroup americanCars = new SelectItemGroup("American Cars");
+        americanCars.setSelectItems(new SelectItem[]{
+            new SelectItem("Chrysler", "Chrysler"),
+            new SelectItem("GM", "GM"),
+            new SelectItem("Ford", "Ford")
+        });
+ 
+        cars.add(germanCars);
+        cars.add(americanCars);
+    }
+ 
+    public List<SelectItem> getCars() {
+        return cars;
+    }
+ 
+    public void setCars(List<SelectItem> cars) {
+        this.cars = cars;
+    }
+ 
+    public String[] getSelectedCars() {
+        return selectedCars;
+    }
+ 
+    public void setSelectedCars(String[] selectedCars) {
+        this.selectedCars = selectedCars;
+    }
+}
