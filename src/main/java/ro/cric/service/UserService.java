0a4f5c8380f5bc5da9317d0372791150c61ebde2
Andrei <andreiherdes@yahoo.com>
2017-05-28 13:37:02 +0300

    Implemented the login and logout features, binded them with the view

--- src/main/java/ro/cric/service/UserService.java
+++ src/main/java/ro/cric/service/UserService.java
@@ -7,5 +7,6 @@ public interface UserService {
 	public User getUserById(long id);
 	public boolean doesEmailExist(String email);
 	public boolean doesUsernameExist(String username, Long id);
+	public User getUserByCredentials(String password, String username);
 	public void register(User user);
 }
